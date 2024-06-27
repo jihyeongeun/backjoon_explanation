9a3b4507b6bbdc62717ec15e24c771287bfb6955
jihyeongeun <hyeongeun0924@gmail.com>
2024-06-27 21:07:07 +0900

    python prac #5

---
+++ 구슬을 나누는 경우의 수.py
@@ -0,0 +1,23 @@
+def solution(balls, share):
+    n = 1
+    sum1 = 1
+    sum2 = 1
+    sum3 = 1
+
+    for i in range(1, (balls - share) + 1):
+        sum1 *= n
+        n += 1
+
+    n = 1
+    for i in range(1, share+1):
+        sum2 *= n
+        n += 1
+
+    n = 1
+    for i in range(1, balls+1):
+        sum3 *= n
+        n += 1
+
+    answer = sum3 / (sum1 * sum2)
+
+    return answer
