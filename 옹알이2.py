b9e2bbf13ad4bf7e774e57914d40961a49395662
jihyeongeun <jidoctor@naver.com>
2024-10-28 23:48:02 +0900

    modify explanation

--- 옹알이2.py
+++ 옹알이2.py
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+        
         for(int i = 0; i < babbling.length; i++){
             if(babbling[i].contains("ayaaya")
                || babbling[i].contains("yeye")
